91c2445fa4a4b4996d61f1ad5e984598e9ecc71f
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-23 13:24:16 +0530

    added client url

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Set this to your frontend URL
+  credentials: true,
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
