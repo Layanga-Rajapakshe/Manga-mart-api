91b0df533e54620ab55ad040db85bb93fdafaf69
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-23 14:01:12 +0530

    updated

--- Backend/utils/createToken.js
+++ Backend/utils/createToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "development", // Secure cookies only in production
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production", // Set to true in production
+    sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 
