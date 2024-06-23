fc0257154b284ca067ab9588a9f1027c7af80b7b
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-06-23 14:05:05 +0530

    UserController updated

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -4,74 +4,71 @@ import bcrypt from "bcryptjs";
 import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
-    const { username, email, password, isAdmin } = req.body;
-  
-    if (!username || !email || !password) {
-      res.status(400);
-      throw new Error("Please fill all the inputs.");
-    }
-  
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).send("User already exists");
-    }
-  
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-  
-    // Ensure isAdmin is included in the new user object
-    const newUser = new User({ username, email, password: hashedPassword, isAdmin });
-  
-    try {
-      await newUser.save();
-      createToken(res, newUser._id);
-  
-      res.status(201).json({
-        _id: newUser._id,
-        username: newUser.username,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-      });
-    } catch (error) {
-      res.status(400);
-      throw new Error("Invalid user data");
-    }
-  });
-  
+  const { username, email, password, isAdmin } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the inputs.");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).send("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword, isAdmin });
+
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({ email });
 
-  if(!existingUser){
+  if (!existingUser) {
     res.status(400);
     throw new Error("Invalid email or password");
   }
 
-  if (existingUser) {
-    const isPasswordValid = await bcrypt.compare(
-      password,
-      existingUser.password
-    );
+  const isPasswordValid = await bcrypt.compare(password, existingUser.password);
 
-    if (isPasswordValid) {
-      createToken(res, existingUser._id);
+  if (isPasswordValid) {
+    createToken(res, existingUser._id);
 
-      res.status(201).json({
-        _id: existingUser._id,
-        username: existingUser.username,
-        email: existingUser.email,
-        isAdmin: existingUser.isAdmin,
-      });
-      return;
-    }
+    res.status(200).json({
+      _id: existingUser._id,
+      username: existingUser.username,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
   }
 });
 
 const logoutCurrentUser = asyncHandler(async (req, res) => {
   res.cookie("jwt", "", {
-    httyOnly: true,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
     expires: new Date(0),
   });
 
